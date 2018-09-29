7e02a47e294a1e97dbe37dd72af6253d5e7245c8
BenOkwara <maranathabenson@gmail.com>
2018-09-29 15:56:50 +0300

    Add the rollone function of if and else

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,23 @@
 var Benson="";
 var Jackton="";
+
+var playTime = function() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+function Player (turn, hisName) {
+  this.roll = 0;
+  this.turnscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.hisName = hisName;
+}
+
+Player.prototype.rollonce= function() {
+  if (this.roll === 1) {
+    this.turnscore = 0;
+    alert("Oops!!" + this.hisName + ", I am Sorry but your score is 1, Start Again.")
+
+  } else {
+    this.turnscore += this.roll;
+  }
+}
